394ef6d445ca75ae3a460fad04e52bf2836a6bd8
Gertjan Assies <g.assies@thenewmotion.com>
2016-10-27 17:09:00 +0200

    error now also is returned as json

--- main.go
+++ main.go
@@ -1,62 +1,67 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "github.com/gorilla/mux"
-  "io"
-  "net"
-  "net/http"
-  "strings"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"io"
+	"net"
+	"net/http"
+	"strings"
 )
 
 type Response struct {
-  Service string     `json:"Service"`
-  Srvs    []*net.SRV `json:"Targets"`
+	Service string     `json:"Service"`
+	Srvs    []*net.SRV `json:"Targets"`
+}
+
+type ErrorResponse struct {
+	Error string
 }
 
 func TrimSuffix(s, suffix string) string {
-  if strings.HasSuffix(s, suffix) {
-    s = s[:len(s)-len(suffix)]
-  }
-  return s
+	if strings.HasSuffix(s, suffix) {
+		s = s[:len(s)-len(suffix)]
+	}
+	return s
 }
 
 func ToJson(service string, srvs []*net.SRV) string {
-  response := &Response{
-    Service: service,
-    Srvs:    srvs,
-  }
-  jsonResponse, _ := json.Marshal(response)
-  return string(jsonResponse)
+	response := &Response{
+		Service: service,
+		Srvs:    srvs,
+	}
+	jsonResponse, _ := json.Marshal(response)
+	return string(jsonResponse)
 }
 
 func lookup(w http.ResponseWriter, r *http.Request) {
-  var host = "marathon.mesos."
-  if strings.HasPrefix(r.Host, "localhost") { // to locally test this i've added 2 srv records to my domain
-    host = "addictivesoftware.net."
-  }
+	var host = "marathon.mesos."
+	if strings.HasPrefix(r.Host, "localhost") { // to locally test this i've added 2 srv records to my domain
+		host = "addictivesoftware.net."
+	}
 
-  service := mux.Vars(r)["service"]
+	service := mux.Vars(r)["service"]
 
-  if _, srvs, err := net.LookupSRV(service, "tcp", host); err != nil {
-    w.Header().Set("Content-Type", "text/plain")
-    io.WriteString(w, fmt.Sprintf("ERROR:%s", err))
-  } else {
-    w.Header().Set("Content-Type", "application/json")
-    io.WriteString(w, ToJson(service, srvs))
-  }
+	w.Header().Set("Content-Type", "application/json")
+	if _, srvs, err := net.LookupSRV(service, "tcp", host); err != nil {
+		errorResponse, _ := json.Marshal(&ErrorResponse{
+			Error: err.Error(),
+		})
+		io.WriteString(w, string(errorResponse))
+	} else {
+		io.WriteString(w, ToJson(service, srvs))
+	}
 }
 
 func status(w http.ResponseWriter, r *http.Request) {
-  io.WriteString(w, "OK")
+	io.WriteString(w, "OK")
 }
 
 func main() {
-  rtr := mux.NewRouter()
-  rtr.HandleFunc("/service/{service}", lookup).Methods("GET")
-  rtr.HandleFunc("/status", status).Methods("GET")
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/service/{service}", lookup).Methods("GET")
+	rtr.HandleFunc("/status", status).Methods("GET")
 
-  http.Handle("/", rtr)
-  http.ListenAndServe(":8000", nil)
+	http.Handle("/", rtr)
+	http.ListenAndServe(":8000", nil)
 }
